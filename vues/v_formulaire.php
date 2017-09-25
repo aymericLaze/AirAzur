1b7441db99051ca50cd40f92cae7519ebe1d7f20
Eugene <etudiant@10.20.162.102>
2017-09-25 12:59:09 +0200

    vue formulaire

--- vues/v_formulaire.php
+++ vues/v_formulaire.php
@@ -2,5 +2,14 @@
 ?>
 <form action="./index.php" method="POST">
     <!-- a faire récuperer -->
+    <label>Nom:<input type="text" name="Nom" value="" /></label><br/>
+    <label>Prenom:<input type="text" name="Prenom" value="" /></label><br/>
+    <label>Adresse:<input type="text" name="Adresse" value="" /></label><br/>
+    <label>Code Postal:<input type="text" name="CP" value="" /></label><br/>
+    <label>Ville:<input type="text" name="Ville" value="" /></label><br/>
+    <label>Téléphone<input type="text" name="NumTel" value="" /></label><br/>
+    <label>Nombre de place à reserver<input type="text" name="Place" value="" /></label><br/>
+    <input type="submit" value="Reserver" name="envoyer" />
+    <input type="reset" value="Effacer" name="reset" />
 </form>
 
