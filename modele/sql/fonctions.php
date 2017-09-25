0c975b860532f350faae24d00e7a7feeee8a9b4e
laze <laze@G14D0S>
2017-09-25 12:46:08 +0200

    ajout de la requête dans getVols()

--- modele/sql/fonctions.php
+++ modele/sql/fonctions.php
@@ -1,23 +1,21 @@
 <?php
     
-    require dirname(__FILE__)."requetesSQL.php";
-    
     //recuperation de la liste des vols
     function getLesVols()
     {
-        //declaration d'un tableau
-        $listeVols = array();
-        
         //appel au fichier permettant la connection  a la bd
         require dirname(__FILE__)."../connction/connectPDO.php";
         
         $connexion = connect();
         
-        if($connexion)
-        {  
-            //requete sql :
-            //select idVols, A1.ville as aeroportDepart, A2.ville as aeroportArrivee, dateDepart, dateArrivee, prix, place
-            //from vols JOIN aeroport as A1 ON vols.aeroportDepart=A1.idAeroport JOIN aeroport as A2 ON vols.aeroportArrivee=A2.idAeroport
-            //where place>0
+        try{
+            //requete sql
+            $sql=   "select idVols, A1.ville as aeroportDepart, A2.ville as aeroportArrivee, dateDepart, dateArrivee, prix, place
+                    from vols JOIN aeroport as A1 ON vols.aeroportDepart=A1.idAeroport JOIN aeroport as A2 ON vols.aeroportArrivee=A2.idAeroport
+                    where place>0";
+            return $resultat = $connexion->query($sql);
+        }
+        catch(PDOException $e){
+            return "Erreur dans la requête ".$e->getMessage();
         }
    }
