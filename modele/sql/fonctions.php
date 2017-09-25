bb869a4fcfb060f547c417f56449abbae77ee678
laze <laze@G14D0S>
2017-09-25 13:00:07 +0200

    ajout de la requête dans getLesVols() affichage des vols recupere

--- modele/sql/fonctions.php
+++ modele/sql/fonctions.php
@@ -9,11 +9,12 @@
         $connexion = connect();
         
         try{
-            //requete sql
-            $sql=   "select idVols, A1.ville as aeroportDepart, A2.ville as aeroportArrivee, dateDepart, dateArrivee, prix, place
+            //requete sql pour recuperer les vols disponible pour la reservation
+            $sql =  "select idVols, A1.ville as aeroportDepart, A2.ville as aeroportArrivee, dateDepart, dateArrivee, prix, place
                     from vols JOIN aeroport as A1 ON vols.aeroportDepart=A1.idAeroport JOIN aeroport as A2 ON vols.aeroportArrivee=A2.idAeroport
                     where place>0";
             return $resultat = $connexion->query($sql);
+            
         }
         catch(PDOException $e){
             return "Erreur dans la requête ".$e->getMessage();
--- vues/v_catalogue.php
+++ vues/v_catalogue.php
@@ -1,12 +1,15 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
 <div id="catalogue">
     Ceci est la page catalogue
+    <table>
+        <!-- modifier le format de l'heure et de la date -->
+        <tr><th>Numéro de vol</th><th>Aéroport de départ</th><th>Aéroport d'arrivée</th><th>Date de départ</th><th>Date d'arrivé</th><th>Prix</th><th>Nombre de places Restantes</th></tr>
+        <?php
+            while($vol = $lesVols->fetch(PDO::FETCH_OBJ))
+            {
+             echo "<tr><td>$vol->idVols</td><td>$vol->aeroportDepart</td><td>$vol->aeroportArrivee</td><td>$vol->dateDepart</td><td>$vol->dateArrivee</td><td>$vol->prix</td><td>$vol->place</td>";
+            }
+        ?>
+    </table>
 </div>
 
