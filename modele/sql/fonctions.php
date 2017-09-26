9e10081c65b15f6300ff956a72f6dece8bcd2a58
laze <laze@G14D0S>
2017-09-26 21:31:37 +0200

    v_catalogue.php et getLesVols() fonctionnel mise a jour du travail fait et de la tdl

--- modele/sql/fonctions.php
+++ modele/sql/fonctions.php
@@ -1,16 +1,21 @@
 <?php
+
     //connexion a la bdd
-    include "connectPDO.php";
-    //recuperation de la liste des vols
+    include "connectPDO.php";  
+    
+   
+    
+    //fonction de recuperation de la liste des vols
     function getLesVols()
     {
         //creation d'un objet PDO
         $connexion = connect();
-
         
         try{
             //creation d'un tableau
             $lesVols = array();
+            //index
+            $i=0;
             
             //requete sql pour recuperer les vols disponible pour la reservation
             $sql =  "select idVols, A1.ville as aeroportDepart, A2.ville as aeroportArrivee, dateDepart, dateArrivee, prix, place
@@ -24,19 +29,18 @@
             //parcours des resultats et stockage dans tableau
             while($vol = $resultatVol->fetch(PDO::FETCH_OBJ))
             {
-                //$lesVols = $vol->idVols;
-                array_push($lesVols,array("1"=>array(
-                                "idVol"=>$vol->idVols,
-                                "aeroportDepart"=>$vol->aeroportDepart,
-                                "aeroportArrivee"=>$vol->aeroportArrivee,
-                                "dateDepart"=>$vol->dateDepart,
-                                "dateArrivee"=>$vol->dateArrivee,
-                                "prix"=>$vol->prix,
-                                "place"=>$vol->place
-                                )
-                            )
-                        );
-                
+                $unVol = array(
+                            "idVol"=>$vol->idVols,
+                            "aeroportDepart"=>$vol->aeroportDepart,
+                            "aeroportArrivee"=>$vol->aeroportArrivee,
+                            "dateDepart"=>$vol->dateDepart,
+                            "dateArrivee"=>$vol->dateArrivee,
+                            "prix"=>$vol->prix,
+                            "place"=>$vol->place
+                            );
+                //ecriture d'un vol dans le tableau a renvoyer
+                $lesVols[$i] = $unVol;
+                $i++;
             }
             return $lesVols;
         }
@@ -44,12 +48,16 @@
             return "Erreur dans la requête ".$e->getMessage();
         }
     }
+    
+
+    
+   //fonction de reservation dans la bdd
    function ajoutReservation($idVols,$nom,$prenom,$adresse,$cp,$ville,$tel,$placeRes,$prixTot){
        $connexion=connect();
        try{
            //requete pour ajouter les réservations dans la BDD
            $sql = "insert into reservation(idVols,nom,prenom,adresse,cp,ville,tel,nbPlaceReservee,prixTotal)"
-               . "values($idVols,$nom,$prenom,$adresse,$cp,$ville,$tel,$placeRes,$prixTot";
+               . "values($idVols,$nom,$prenom,$adresse,$cp,$ville,$tel,$placeRes,$prixTot)";
            $connexion->query($sql);
        }
        catch(PDOException $e){
--- vues/v_catalogue.php
+++ vues/v_catalogue.php
@@ -1,43 +1,33 @@
 
 <div id="catalogue">
+    <!-- Catalogue des vols disponible -->
     <table>
-        <!-- modifier le format de l'heure et de la date -->
+        <!-- Entete du tableau -->
         <tr><th>Numéro de vol</th><th>Aéroport de départ</th><th>Aéroport d'arrivée</th><th>Date de départ</th><th>Date d'arrivée</th><th>Prix</th><th>Nombre de places Restantes</th><th>Réserver pour ce vol</th></tr>
-        <!--<?php/*
-            while($vol = $lesVols->fetch(PDO::FETCH_OBJ))
-            {
-                $numVol = $vol->idVols;
-        */?>
-        <tr>
-            <td><?php// echo $numVol ?></td>
-            <td><?php// echo $vol->aeroportDepart?></td>
-            <td><?php// echo $vol->aeroportArrivee?></td>
-            <td><?php// echo $vol->dateDepart?></td>
-            <td><?php// echo $vol->dateArrivee?></td>
-            <td><?php// echo $vol->prix ?></td>
-            <td><?php// echo $vol->place?></td>
-            <td><a href='index.php?action=formulaire&vol=<?php// echo $numVol ?>'>Réserver</a> </td>
-        </tr>
-        <?php/*
-            }
-        */?>-->
+        
+        <!-- Affichage du tableau -->
         <?php
-            foreach($lesVols as $vol)
+            //Parcours du tableau, contenant les vols, recupere
+            foreach($lesVols as $indice=>$vol)
             {
-                ?>
+        ?>
+        
+        <!-- Corps du tableau -->
         <tr>
-            <td><?php echo $vol["1"]["idVols"] ?></td>
-            <td><?php echo $vol->aeroportDepart?></td>
-            <td><?php echo $vol->aeroportArrivee?></td>
-            <td><?php echo $vol->dateDepart?></td>
-            <td><?php echo $vol->dateArrivee?></td>
-            <td><?php echo $vol->prix ?></td>
-            <td><?php echo $vol->place?></td>
-            <td><a href='index.php?action=formulaire&vol=<?php echo $numVol ?>'>Réserver</a> </td>
+            <td><?php echo $vol["idVol"] ?></td>
+            <td><?php echo $vol["aeroportDepart"] ?></td>
+            <td><?php echo $vol["aeroportArrivee"] ?></td>
+            <td><?php echo $vol["dateDepart"] ?></td>
+            <td><?php echo $vol["dateArrivee"] ?></td>
+            <td><?php echo $vol["prix"]." ?" ?></td>
+            <td><?php echo $vol["place"] ?></td>
+            <td><a href='index.php?action=formulaire&vol=<?php echo $vol["idVol"] ?>'>Réserver</a> </td>
         </tr>
+        
         <?php
             }
         ?>
+        
     </table>
 </div>
 
