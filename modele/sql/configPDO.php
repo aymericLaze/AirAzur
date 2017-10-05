5f5e9112d170636f773ba48e6efed9e588f14774
MAINENTI <MAINENTI@10.20.160.10>
2017-10-05 09:57:19 +0200

    ajout fonction PDF+test modif index création vue pdf

--- modele/sql/configPDO.php
+++ modele/sql/configPDO.php
@@ -1,7 +1,7 @@
 <?php
     //constante de connexion a la base de donnee
     define('HOST','localhost');
-    define('DBNAME','alaze');
-    define('USER','alaze');
-    define('MDP','alaze');
+    define('DBNAME','airazur');
+    define('USER','root');
+    define('MDP','');
 ?>
