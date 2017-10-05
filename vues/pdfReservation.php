22dfadbf20290f0a13f934f9f08a1cb45360380b
MAINENTI <MAINENTI@10.20.160.10>
2017-10-05 14:43:01 +0200

    commentaire pdf

--- vues/pdfReservation.php
+++ vues/pdfReservation.php
@@ -11,19 +11,23 @@ function creerPDF($uneReservations,$unVol) {
     $pdf->AddPage();
 // définit la police courante
     $pdf->SetFont('Arial', 'B', 16);
-//ajoute une image
+//Header
     $pdf->Image('./images/avion.jpg', 10, 10, 64, 48);
     $pdf->Cell(80);
-    // Titre
     $pdf->Cell(0, 10, 'Air Azur', 0, 1, 'C');
     $pdf->Ln(20);
     $pdf->Cell(80);
+//Titre
     $pdf->Cell(0, 10, 'Récapitulatif de reservation', 0, 1, 'C');
     $pdf->Ln(20);
+//Affiche le numero de reservation et le cadre
     $pdf->Cell(40, 10, utf8_decode("Réservation n°" . $uneReservations["idReservation"]), 0, 0);
     $pdf->Line($pdf->GetX()+5, $pdf->GetY() + 5, $pdf->GetX() + 160, $pdf->GetY() + 5);
     $pdf->Ln();
-    $pdf->Line($pdf->GetX(), $pdf->GetY() - 5, $pdf->GetX(), $pdf->GetY() + 200);
+    $pdf->Line($pdf->GetX(), $pdf->GetY() - 5, $pdf->GetX(), $pdf->GetY() + 300);
+//Affiche le informations du vol
+    $pdf->Cell(0);
+    $pdf->Ln(10);
     $pdf->Cell(0, 10, "Vol : " . $unVol["idVols"], 0, 0, 'C');
     $pdf->Ln(10);
     $pdf->Cell(0, 10, utf8_decode("Départ : " . $unVol["aeroportDepart"]. " ".$unVol["dateDepart"]), 0, 0, 'C');
@@ -32,6 +36,7 @@ function creerPDF($uneReservations,$unVol) {
     $pdf->Ln(10);
     $pdf->Cell(0);
     $pdf->Ln(10);
+//Affiche les informations de la reservation
     $pdf->Cell(0, 10, utf8_decode("Nom : " . $uneReservations["nomClient"] . " " . $uneReservations["prenomClient"]), 0, 0, 'C');
     $pdf->Ln(10);
     $pdf->Cell(0, 10, utf8_decode("Adresse : " . $uneReservations["adresseClient"] . " " . $uneReservations["codePostalClient"] . " " . $uneReservations["villeClient"]), 0, 0, 'C');
@@ -39,7 +44,7 @@ function creerPDF($uneReservations,$unVol) {
     $pdf->Cell(0, 10, "Places : " . $uneReservations["nbPlaceReservee"], 0, 0, 'C');
     $pdf->Ln(10);
     $pdf->Cell(0, 10, utf8_decode("Prix : " . $uneReservations["prixTotal"] . utf8_encode(chr(128))), 0, 0, 'C');
-    //ouverture sur le navigateur
+//ouverture sur le navigateur
     $pdf->Output();
     ob_end_flush();
 }
